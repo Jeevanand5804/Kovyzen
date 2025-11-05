45ace0fd44a657e86c2390c0ec654283830fd9cb
Jeevanand5804 <jeevanand5804@gmail.com>
2025-11-05 07:13:00 +0530

    modified

--- LMS/src/context/AppContext.jsx
+++ LMS/src/context/AppContext.jsx
@@ -2,7 +2,6 @@ import { createContext,useEffect,useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import {useNavigate} from 'react-router-dom';
 import humanizeDuration from 'humanize-duration';
-import { useAuth,useUser } from "@clerk/clerk-react";
 
 export const AppContext=createContext()
 
@@ -11,8 +10,6 @@ export const AppContextProvider= (props)=>{
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
 
-    const {getToken}=useAuth()
-    const {user}=useUser()
 
 const [allCourses, setAllCourses] = useState([]);
 const [isEducator, setIsEducator] = useState(true);
@@ -70,16 +67,6 @@ useEffect(()=>{
     fetchAllcourses()
     fetchEnrolledCourses()
 },[])
-
-const logToken=async()=>{
-    console.log(await getToken());
-}
-
-useEffect(()=>{
-    if(user){
-        logToken();
-    }
-},[user])
 
 const value={
     currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration,calculateChapterTime,enrolledCourses,fetchEnrolledCourses
--- server/server.js
+++ server/server.js
@@ -2,26 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
-import educatorRouter from './routes/educatorRoutes.js';
-import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 await connectDB();
 
 //Middleware
 app.use(cors());
-app.use(express.json());
-app.use(clerkMiddleware);
 
 // Routes
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
-app.post('/clerk',express.json(),clerkWebhooks)
-
-app.use('/api/educator',express.json(),educatorRouter);
 
 const PORT = process.env.PORT || 5000;
 
